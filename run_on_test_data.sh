4d758946984204d7c3407c0b680f80aa210b7df6
Maximilian Huber <maximilian.huber@tngtech.com>
2024-04-04 22:52:14 +0200

    wip
    Signed-off-by: Maximilian Huber <maximilian.huber@tngtech.com>

---
+++ run_on_test_data.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+set -x
+poetry run python -m package \
+  --src test_data/hello-2.12 \
+  --src_strip_prefix /src/ \
+  --artifact test_data/hello-2.12/hello \
+  _test_out
